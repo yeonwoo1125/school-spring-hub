2aaea5d64ee0cc63cdd2a62e1fc6911ca528f555
s2021 <s2021@e-mirim.hs.kr>
2022-04-18 11:18:44 +0900

    execution 실습

---
+++ Spring_core/Execution/src/main/java/kr/hs/study/beans/TestBean1.java
@@ -0,0 +1,18 @@
+package kr.hs.study.beans;
+
+public class TestBean1 {
+	public void method1() {
+		System.out.println("kr.hs.study.veabs.TestBean1의 method1");
+	}
+	public void method2() {
+		System.out.println("kr.hs.study.veabs.TestBean1의 method2");
+	}
+
+	public void method1(int a1) {
+		System.out.println("int 매개변수 method1");
+	}
+	
+	public void method1(int a, int b) {
+		System.out.println("int 매개변수 두개 method1");
+	}
+}
---
+++ Spring_core/Execution/src/main/java/kr/hs/study/main/MainClass.java
@@ -0,0 +1,19 @@
+package kr.hs.study.main;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import kr.hs.study.beans.TestBean1;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("kr/hs/study/config/config.xml");
+		
+		TestBean1 t1 = ctx.getBean("t1",TestBean1.class);
+		t1.method1();
+		t1.method1(1, 1);
+		ctx.close();
+
+	}
+
+}
