2f6e7e0ca5a948468bc997ada0905eefb3a94ed0
s2021 <s2021@e-mirim.hs.kr>
2022-04-20 14:00:20 +0900

    Exectuion 실습

--- Spring_core/Execution/src/main/java/kr/hs/study/beans/TestBean1.java
+++ Spring_core/Execution/src/main/java/kr/hs/study/beans/TestBean1.java
@@ -2,10 +2,10 @@ package kr.hs.study.beans;
 
 public class TestBean1 {
 	public void method1() {
-		System.out.println("kr.hs.study.veabs.TestBean1의 method1");
+		System.out.println("kr.hs.study.beans.TestBean1의 method1");
 	}
 	public void method2() {
-		System.out.println("kr.hs.study.veabs.TestBean1의 method2");
+		System.out.println("kr.hs.study.beans.TestBean1의 method2");
 	}
 
 	public void method1(int a1) {
@@ -14,5 +14,12 @@ public class TestBean1 {
 	
 	public void method1(int a, int b) {
 		System.out.println("int 매개변수 두개 method1");
+	}
+	public void method1(int a, String b) {
+		System.out.println("int 매개변수, String 매개변수 method1");
+	}
+	public int method3() {
+		System.out.println("int 리턴 method3");
+		return 1;
 	}
 }
--- Spring_core/Execution/src/main/java/kr/hs/study/main/MainClass.java
+++ Spring_core/Execution/src/main/java/kr/hs/study/main/MainClass.java
@@ -10,8 +10,15 @@ public class MainClass {
 		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("kr/hs/study/config/config.xml");
 		
 		TestBean1 t1 = ctx.getBean("t1",TestBean1.class);
+		/*
+		 * t1.method1(); t1.method1(1, 1);
+		 */
+		t1.method2();
+		t1.method1(1, "spring");
+		t1.method1(1, 2);
+		t1.method1(1);
 		t1.method1();
-		t1.method1(1, 1);
+		t1.method3();
 		ctx.close();
 
 	}
